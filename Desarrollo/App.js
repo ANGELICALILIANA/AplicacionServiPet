4b95016e481ddb27d6a670f6abe57992bbb6c737
oscaralvarado <ofalvarado8@misena.edu.co>
2020-08-22 13:46:38 -0500

    Sabado Sesion 1
    Sabado Sesion 1

--- Desarrollo/App.js
+++ Desarrollo/App.js
@@ -1,36 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { StyleSheet, Text, View, Button, ImageBackground } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Hometwo from "./app/Componets/hometwo.js";
+import Headerhometwo from "./app/Componets/headerHometwo.js";
 
-
-function HomeScreen() {
-  return (
-    <Hometwo/>
+function HeaderhometwoScreen(){
+  return(
+<Headerhometwo/>
   );
 }
+function HometwoScreen() {
+  return(
+    <Hometwo/>
+    );
+  } 
 
 const Stack = createStackNavigator();
 
 function App() {
   return (
     <NavigationContainer>
+      
       <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
+      <Stack.Screen name=" " component={HometwoScreen}/>
+      <Stack.Screen name="  " component={HeaderhometwoScreen}/>
       </Stack.Navigator>
+      
     </NavigationContainer>
   );
 }
 
 export default App;
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
