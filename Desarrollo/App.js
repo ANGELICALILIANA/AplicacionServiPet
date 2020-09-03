d8c73f53375997497a9416d7ba7f3545cfa6478d
oscaralvarado <ofalvarado8@misena.edu.co>
2020-09-02 21:08:45 -0500

    Sesion 02-09
    Commit

--- Desarrollo/App.js
+++ Desarrollo/App.js
@@ -1,18 +1,21 @@
 import * as React from 'react';
+/* import React, { Component } from 'react'; */
 import { StyleSheet, Text, View, Button, ImageBackground } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+/* import { DrawerNavigator } from 'react-native'; */
+import * as Font from 'expo-font';
 import Hometwo from "./app/Componets/hometwo.js";
 import Headerhometwo from "./app/Componets/headerHometwo.js";
-
+/* import Estetica from "./app/Componets/esteticaScreen.js"
+import Veterinaria from "./app/Componets/veterinariaScreen.js" */
 
 function HometwoScreen() {
   return(
     <Hometwo/>
     );
   } 
-
-
+  
 const Stack = createStackNavigator();
 
 function App() {
@@ -24,9 +27,20 @@ function App() {
       <Stack.Navigator headerMode="none">
       <Stack.Screen name=" " component={HometwoScreen}/>
       </Stack.Navigator>
+      
     </NavigationContainer>
   );
 }
+
+/* const navegacion = DrawerNavigator({
+  estetica:{
+    screen: Estetica
+  },
+  veterinaria:{
+    screen: Veterinaria
+  },
+
+}) */
 
 export default App;
 
---
+++ Desarrollo/app/Componets/esteticaScreen.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import { Icon, Button, Container, Header, Content, Left} from 'native-base';
+
+class estiticaScreem extends Component{
+    render(){
+        return(
+            <Container>
+                <Content>
+                    <Text>ESTETICA Y PELUQUERIA</Text>
+                </Content>
+            </Container>
+        )
+    }
+}
+export default estiticaScreem;
